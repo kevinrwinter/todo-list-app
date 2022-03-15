4ca70e0730c40cca4166e30d5cbc60b71a84e1a8
Winter Kevin <s2200136@KW-MacBook-Air.home>
2022-03-15 21:37:52 +0200

    Minor fixes

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const createTodoLi = (todo) => {
     <li class="todos-list-item">
     <span>${todo}</span>
     <div class="icon-group">
-      <i class="fa-regular fa-square done"></i>
+      <i class="fa-regular fa-square not-done"></i>
       <i class="fa-regular fa-trash-can delete"></i>
     </div>
   `;
@@ -35,9 +35,14 @@ todosList.addEventListener("click", (e) => {
   if (e.target.classList.contains("delete")) {
     e.target.parentElement.parentElement.remove();
   }
-  // Mark todo item as complete
-  if (e.target.classList.contains("done")) {
+  // Mark todo item as complete or incomplete
+  if (e.target.classList.contains("not-done")) {
     e.target.classList.replace("fa-square", "fa-square-check");
     e.target.parentElement.previousElementSibling.classList.add("complete");
+    e.target.classList.replace("not-done", "done");
+  } else if (e.target.classList.contains("done")) {
+    e.target.classList.replace("fa-square-check", "fa-square");
+    e.target.parentElement.previousElementSibling.classList.remove("complete");
+    e.target.classList.replace("done", "not-done");
   }
 });
