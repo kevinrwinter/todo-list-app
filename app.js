8284e5c24b947580a91cc97d6eea4d25f83b905a
Winter Kevin <s2200136@KW-MacBook-Air.home>
2022-03-15 19:45:07 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,43 @@
+const addTodo = document.querySelector(".add-todo");
+const todosList = document.querySelector(".todos-list");
+const hiddenLine = document.querySelector("#hidden-line");
+
+const createTodoLi = (todo) => {
+  const templateHTML = `
+    <li class="todos-list-item">
+    <span>${todo}</span>
+    <div class="icon-group">
+      <i class="fa-regular fa-square done"></i>
+      <i class="fa-regular fa-trash-can delete"></i>
+    </div>
+  `;
+
+  todosList.innerHTML += templateHTML;
+};
+
+// Add todos
+addTodo.addEventListener("submit", (e) => {
+  e.preventDefault();
+  // Show "mark as complete or delete" paragraph
+  hiddenLine.classList.remove("hide");
+  // Remove space characters
+  const todo = addTodo.add.value.trim();
+  // Prevent empty items
+  if (todo.length) {
+    createTodoLi(todo);
+    addTodo.reset();
+  }
+});
+
+// Modify todos
+todosList.addEventListener("click", (e) => {
+  // Delete todo item
+  if (e.target.classList.contains("delete")) {
+    e.target.parentElement.parentElement.remove();
+  }
+  // Mark todo item as complete
+  if (e.target.classList.contains("done")) {
+    e.target.classList.replace("fa-square", "fa-square-check");
+    e.target.parentElement.previousElementSibling.classList.add("complete");
+  }
+});
